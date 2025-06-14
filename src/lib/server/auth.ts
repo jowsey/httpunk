19ff0ca5de4516fe145f84c56456bde2fe657d3d
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2025-06-14 11:51:27 +0100

    update deps & fix formatting
    my old ass vscode config was using the Deno formatter instead of prettier -_-

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,16 +1,16 @@
-import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } from "$env/static/private";
-import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "./db";
+import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } from '$env/static/private';
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { db } from './db';
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
-		provider: "sqlite",
+		provider: 'sqlite'
 	}),
 	socialProviders: {
 		discord: {
 			clientId: DISCORD_CLIENT_ID as string,
-			clientSecret: DISCORD_CLIENT_SECRET as string,
-		},
-	},
+			clientSecret: DISCORD_CLIENT_SECRET as string
+		}
+	}
 });
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,10 @@
-import { drizzle } from "drizzle-orm/bun-sqlite";
-import { Database } from "bun:sqlite";
-import * as schema from "./schema/better-auth";
+import { drizzle } from 'drizzle-orm/bun-sqlite';
+import { Database } from 'bun:sqlite';
+import * as schema from './schema/better-auth';
 
-if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 const client = new Database(process.env.DATABASE_URL);
-client.run("PRAGMA journal_mode = WAL;");
+client.run('PRAGMA journal_mode = WAL;');
 
 export const db = drizzle(client, { schema });
