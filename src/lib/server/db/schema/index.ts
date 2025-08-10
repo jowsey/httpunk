0bdd58eb591922250e12517a236f9f2585d5a7aa
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2025-08-10 16:51:46 +0100

    Remove unnecessary commented check
    postgres enum handles this now

--- src/lib/server/db/schema/index.ts
+++ src/lib/server/db/schema/index.ts
@@ -4,18 +4,14 @@ export * from './better-auth';
 
 export const pronounsEnum = pgEnum('pronouns', ['he', 'they', 'she']);
 
-export const character = pgTable(
-	'character',
-	{
-		id: uuid('id').primaryKey().defaultRandom(),
-		userId: text('user_id')
-			.notNull()
-			.references(() => user.id),
-		name: text('name').notNull(),
-		pronouns: pronounsEnum().notNull(),
-		level: integer('level').notNull().default(1),
-		exp: integer('exp').notNull().default(0),
-		createdAt: timestamp('created_at').notNull().defaultNow()
-	}
-	// (table) => [check('pronouns_check1', sql`${table.pronouns} IN ('he', 'they', 'she')`)] // db:push sorta breaks with any of these checks rn, todo fix
-);
+export const character = pgTable('character', {
+	id: uuid('id').primaryKey().defaultRandom(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => user.id),
+	name: text('name').notNull(),
+	pronouns: pronounsEnum().notNull(),
+	level: integer('level').notNull().default(1),
+	exp: integer('exp').notNull().default(0),
+	createdAt: timestamp('created_at').notNull().defaultNow()
+});
