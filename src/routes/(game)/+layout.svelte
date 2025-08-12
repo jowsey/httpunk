9d7ff84886a534c54f71cca58a8c39c5453caa91
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2025-08-12 20:36:36 +0100

    Move client websocket to class & allow clicking through footer

--- src/routes/(game)/+layout.svelte
+++ src/routes/(game)/+layout.svelte
@@ -3,12 +3,7 @@
 	import { dev } from '$app/environment';
 	import { page } from '$app/state';
 	import { appState } from '$lib/client/state.svelte';
-	import {
-		WebsocketMessageType,
-		type CharacterExpUpdateMessage,
-		type CharacterLevelUpdateMessage,
-		type WebsocketMessage
-	} from '$lib/shared-types/websocket-message';
+	import { WebsocketClient } from '$lib/client/WebsocketHandler.svelte';
 	import NavButton from '$lib/components/NavButton.svelte';
 
 	import 'overlayscrollbars/overlayscrollbars.css';
@@ -17,62 +12,17 @@
 
 	appState.character = data.character;
 
-	let wsState: number | null = $state(null);
+	let ws: WebsocketClient | null = $state(null);
 
 	onMount(() => {
-		if (!data.session) {
-			return;
-		}
+		if (!data.session) return;
 
 		const url = dev ? `ws://localhost:3002/api/ws` : `ws://${window.location.host}/api/ws`;
 		console.log(`[ws] connecting to ${url}`);
 
-		const ws = new WebSocket(url);
-
-		ws.onopen = () => {
-			wsState = ws.readyState;
-			console.log('[ws] connected');
-		};
-
-		ws.onmessage = (event) => {
-			console.log('[ws] message:', event.data);
-
-			const msgData = JSON.parse(event.data) as WebsocketMessage;
-
-			switch (msgData.type) {
-				case WebsocketMessageType.CHARACTER_EXP_UPDATE: {
-					const expUpdate = msgData as CharacterExpUpdateMessage;
-					if (expUpdate.characterId === appState.character?.id) {
-						appState.character.exp = expUpdate.exp;
-					}
-					break;
-				}
-				case WebsocketMessageType.CHARACTER_LEVEL_UPDATE: {
-					const levelUpdate = msgData as CharacterLevelUpdateMessage;
-					if (levelUpdate.characterId === appState.character?.id) {
-						appState.character.level = levelUpdate.level;
-					}
-					break;
-				}
-				default: {
-					console.warn(`[ws] unknown message type: ${msgData.type}`);
-					break;
-				}
-			}
-		};
-
-		ws.onerror = (error) => {
-			wsState = ws.readyState;
-			console.error('[ws] error:', error);
-		};
-
-		ws.onclose = () => {
-			wsState = ws.readyState;
-			console.log('[ws] connection closed');
-		};
+		ws = new WebsocketClient(url);
 
 		return () => {
-			console.log('[ws] closing connection');
 			ws?.close();
 		};
 	});
@@ -112,31 +62,33 @@
 
 <!-- bottom nav menu -->
 <div
-	class="fixed right-0 bottom-0 flex h-12 w-dvw items-center justify-end gap-x-4 bg-gradient-to-b from-transparent to-neutral-950 px-2 select-none"
+	class="pointer-events-none fixed bottom-0 flex h-12 w-dvw justify-end bg-gradient-to-b from-transparent to-neutral-950 select-none"
 >
-	<p class="text-sm">
-		ws {wsState === WebSocket.OPEN ? 'connected' : 'disconnected'}
-	</p>
+	<div class="pointer-events-auto flex h-full items-center justify-end gap-x-4 px-4">
+		<p class="text-sm">
+			ws {ws?.readyState === WebSocket.OPEN ? 'connected' : 'disconnected'}
+		</p>
 
-	<div class="flex items-center gap-x-2">
-		<NavButton icon="/svg/squares-four-fill.svg" href="/hub" label="Hub" />
-		<NavButton icon="/svg/person-arms-spread-fill.svg" href="/character" label="Character" />
-	</div>
-
-	<a
-		class={[
-			'group hover:bg-brand flex h-8 items-center justify-center gap-x-2 rounded-full border border-neutral-900 bg-neutral-950 px-[1px] transition-all duration-75 hover:text-neutral-900',
-			page.url.pathname === '/profile' && '!bg-brand !text-neutral-900'
-		]}
-		href="/profile"
-	>
-		<p class="pl-3.5 max-sm:hidden">{data.session?.user.name}</p>
-		<div class="size-7">
-			<img
-				class="size-full rounded-full border border-transparent transition-transform group-hover:scale-110"
-				src={data.session?.user.image}
-				alt="Profile icon"
-			/>
+		<div class="flex items-center gap-x-2">
+			<NavButton icon="/svg/squares-four-fill.svg" href="/hub" label="Hub" />
+			<NavButton icon="/svg/person-arms-spread-fill.svg" href="/character" label="Character" />
 		</div>
-	</a>
+
+		<a
+			class={[
+				'group hover:bg-brand flex h-8 items-center justify-center gap-x-2 rounded-full border border-neutral-900 bg-neutral-950 px-[1px] transition-all duration-75 hover:text-neutral-900',
+				page.url.pathname === '/profile' && '!bg-brand !text-neutral-900'
+			]}
+			href="/profile"
+		>
+			<p class="pl-3.5 max-sm:hidden">{data.session?.user.name}</p>
+			<div class="size-7">
+				<img
+					class="size-full rounded-full border border-transparent transition-transform group-hover:scale-110"
+					src={data.session?.user.image}
+					alt="Profile icon"
+				/>
+			</div>
+		</a>
+	</div>
 </div>
