29e430deeae850efce6fa2a821eeac2a7066174c
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2025-08-13 02:24:40 +0100

    Improve responsiveness of avatar/equipment grid
    still not perfect but I don't know how I can possibly shrink it anymore
    
    i think at some point we're just gonna have to say sorry to the iPhone 4 users and move on

--- src/routes/(game)/character/+page.svelte
+++ src/routes/(game)/character/+page.svelte
@@ -43,58 +43,78 @@
 	</div>
 </div>
 
-<div class="mx-auto my-16 flex w-fit">
-	<!-- character image -->
-	<div class="relative h-160 w-80 shrink-0">
-		<img
-			id="avatar"
-			class="size-full object-contain brightness-125 contrast-125 saturate-125 select-none"
-			src="render/avatar_glass.webp"
-			alt="character"
-		/>
-
-		<div class="absolute top-12 left-40">
-			<CalloutLine firstOffset={{ x: 24, y: -24 }} secondOffset={{ x: 138, y: -24 }} label="Head" />
-		</div>
-
-		<div class="absolute top-40 left-40">
-			<CalloutLine firstOffset={{ x: 24, y: -24 }} secondOffset={{ x: 178, y: -24 }} label="Chest" />
-		</div>
-
-		<div class="absolute top-56 left-64">
-			<CalloutLine firstOffset={{ x: 24, y: 24 }} secondOffset={{ x: 122, y: 24 }} label="Arms" />
-		</div>
-
-		<div class="absolute top-74 left-10">
-			<CalloutLine firstOffset={{ x: 64, y: 64 }} secondOffset={{ x: 338, y: 64 }} label="Hands" />
-		</div>
-
-		<div class="absolute top-112 left-48">
-			<CalloutLine firstOffset={{ x: 24, y: 24 }} secondOffset={{ x: 146, y: 24 }} label="Legs" />
-		</div>
-
-		<div class="absolute top-140 left-34">
-			<CalloutLine firstOffset={{ x: 24, y: 24 }} secondOffset={{ x: 154, y: 24 }} label="Feet" />
-		</div>
-	</div>
-
-	<!-- equipment grid -->
-	<div class="flex flex-col gap-y-8">
-		<EquipmentSet></EquipmentSet>
-		<EquipmentSet class="ml-10"></EquipmentSet>
-		<EquipmentSet class="ml-20"></EquipmentSet>
-		<EquipmentSet class="ml-20"></EquipmentSet>
-		<EquipmentSet class="ml-10"></EquipmentSet>
-		<EquipmentSet></EquipmentSet>
-	</div>
-</div>
-
-<div class="flex w-full justify-end gap-x-2">
+<div class="mt-4 mb-8 flex w-full justify-end gap-x-2">
 	<StyledButton rounded="left" theme="good" size="compact" onclick={addExp}>Give 5 EXP</StyledButton>
 	<!-- we technically dont need a form here but it causes svelte to reload data on submit which we'd be doing anyway since we wanna redirect to character creation -->
 	<form method="POST" action="?/deleteCharacters" use:enhance>
 		<StyledButton rounded="right" theme="danger" size="compact" type="submit">Delete character</StyledButton>
 	</form>
+</div>
+
+<!-- character -->
+<div class="flex w-full flex-wrap justify-between gap-8">
+	<!-- equipment -->
+	<div class="flex w-fit px-4 max-md:w-full md:mx-auto">
+		<!-- avatar -->
+		<div
+			class="relative h-160 w-80 shrink-0 *:not-first:absolute *:not-first:opacity-0 *:not-first:transition-all *:not-first:md:opacity-100"
+		>
+			<img
+				draggable="false"
+				id="avatar"
+				class="size-full object-contain brightness-125 contrast-125 saturate-125 select-none"
+				src="render/avatar_glass.webp"
+				alt="yellow avatar 3D render"
+			/>
+
+			<div class="top-12 left-40">
+				<CalloutLine firstOffset={{ x: 24, y: -24 }} secondOffset={{ x: 138, y: -24 }} label="Head" />
+			</div>
+
+			<div class="top-40 left-40">
+				<CalloutLine firstOffset={{ x: 24, y: -24 }} secondOffset={{ x: 178, y: -24 }} label="Chest" />
+			</div>
+
+			<div class="top-56 left-64">
+				<CalloutLine firstOffset={{ x: 24, y: 24 }} secondOffset={{ x: 122, y: 24 }} label="Arms" />
+			</div>
+
+			<div class="top-74 left-10">
+				<CalloutLine firstOffset={{ x: 64, y: 64 }} secondOffset={{ x: 338, y: 64 }} label="Hands" />
+			</div>
+
+			<div class="top-112 left-48">
+				<CalloutLine firstOffset={{ x: 24, y: 24 }} secondOffset={{ x: 146, y: 24 }} label="Legs" />
+			</div>
+
+			<div class="top-140 left-34">
+				<CalloutLine firstOffset={{ x: 24, y: 24 }} secondOffset={{ x: 154, y: 24 }} label="Feet" />
+			</div>
+		</div>
+
+		<!-- equipment grid -->
+		<div class="right-8 flex flex-col gap-y-8 drop-shadow-2xl drop-shadow-neutral-950 *:transition-all max-md:absolute">
+			<EquipmentSet />
+			<EquipmentSet class="md:ml-10" />
+			<EquipmentSet class="md:ml-20" />
+			<EquipmentSet class="md:ml-20" />
+			<EquipmentSet class="md:ml-10" />
+			<EquipmentSet />
+		</div>
+	</div>
+
+	<!-- stats (placeholder) -->
+	<div class="flex min-w-64 grow flex-col">
+		<p class="text-2xl font-bold">Overview</p>
+		<div class="mt-4 flex flex-col gap-y-2">
+			<p class="text-sm text-neutral-400">Luck: 10</p>
+			<p class="text-sm text-neutral-400">Skill: 20</p>
+			<p class="text-sm text-neutral-400">Will: 15</p>
+			<p class="text-sm text-neutral-400">Pleasure: 5</p>
+			<p class="text-sm text-neutral-400">Pain: 50</p>
+			<p class="text-sm text-neutral-400">Reason: 100</p>
+		</div>
+	</div>
 </div>
 
 <style lang="postcss">
