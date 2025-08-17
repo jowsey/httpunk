91da9f7592e5a4787f5ae25ad8ee03aeee6d1d57
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2025-08-17 22:47:32 +0100

    Expose package.version to client

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { version } from './package.json';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	define: {
+		'import.meta.env.VERSION': JSON.stringify(version)
+	}
 });
