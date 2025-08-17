74837619ecca93176abfa113e8c2288b6996a064
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2025-08-17 22:55:34 +0100

    Conditionally import Better-Auth SvelteKit plugin
    non-sveltekit environments (core) can't import from $app/server and it shouldn't have any effect there anyway to my knowledge so i think this is fine

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,11 +1,24 @@
-import { betterAuth } from 'better-auth';
+import { betterAuth, type BetterAuthPlugin } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { sveltekitCookies } from 'better-auth/svelte-kit';
 import { db } from './db';
+import chalk from 'chalk';
+
+const plugins: BetterAuthPlugin[] = [];
+
+try {
+	// if we're in sveltekit, use the sveltekit plugin, otherwise it's fine
+	// todo this might not scale nicely if we need more plugins
+	const { getRequestEvent } = await import('$app/server');
+	plugins.push(sveltekitCookies(getRequestEvent));
+	console.log(chalk.green(`Using sveltekitCookies() Better-Auth plugin. This is expected in vite & web.`));
+} catch {
+	// todo find a way to figure out if we're in core and either silent or error
+	console.log(chalk.blue(`Skipping sveltekitCookies() Better-Auth plugin. This is expected in core.`));
+}
 
 export const auth = betterAuth({
-	// todo come back to this when better-auth get their shit together
-	// https://www.better-auth.com/docs/integrations/svelte-kit#server-action-cookies
-	// plugins: [sveltekitCookies(getRequestEvent)],
+	plugins,
 	database: drizzleAdapter(db, {
 		provider: 'pg'
 	}),
